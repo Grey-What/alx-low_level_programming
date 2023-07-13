506018b5e621692363788715d0f0e990b0c56be0
Nick <greywhat2002@gmail.com>
2023-07-13 10:59:24 -0700

    made betty happy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+
+/* this function prints string of text */
 int main(void)
 {
 	char outstr[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(outstr);
 	return (0);
 }
