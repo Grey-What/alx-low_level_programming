0ed55adf7bd385ab45fad82fb78098adf9a7d824
Nick <greywhat2002@gmail.com>
2023-07-13 08:26:31 -0700

    fixed function in main.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    return (0);
+	printf("this is my first function\n");
+	return(0);
 }
