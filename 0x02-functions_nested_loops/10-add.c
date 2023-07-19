124d9caef6e2ccdefd8cc09c1c4e36d55c8e86c8
Nick <greywhat2002@gmail.com>
2023-07-19 06:03:36 -0700

    program adds two ints and print the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add -this function adds two ints and print results
+ *
+ * @a: this is the first int value
+ * @b: this is the second int value
+ *
+ * Description: thi function prints the sum of two int values
+ *
+ * Return: this function returns 0
+ */
+int add(int a, int b)
+{
+	int ans = a + b;
+
+	return (ans);
+}
