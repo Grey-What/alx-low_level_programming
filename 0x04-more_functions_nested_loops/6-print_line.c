19dd42c42348d20883c92dbdb027a3404d29fa30
Nick <greywhat2002@gmail.com>
2023-07-20 08:16:52 -0700

    changed code to inclue events when n is < 0

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,19 @@
  */
 void print_line(int n)
 {
-	int i = 1;
-
-	while (i < n && !(n <= 0))
+	if (n <= 0)
 	{
-		_putchar('_');
-		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		int i = 1;
+
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+		_putchar('\n');
+	}
 }
