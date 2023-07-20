5820d96c55afe04bfef18970b8175270aadb67dc
Nick <greywhat2002@gmail.com>
2023-07-20 07:19:45 -0700

    function returns the multiplication sum of two int values

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - function multiplies two integers
+ *
+ * @a: this is the first value to be multiplied
+ * @b: this is the second value to be multiplied
+ *
+ * Description: this function multiplies two values and returns the answer
+ *
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
