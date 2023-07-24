21c16ba101eb084a983332b24a8d5d402c150014
Nick <greywhat2002@gmail.com>
2023-07-24 06:39:24 -0700

    this function accepts a pointer as parameter and changes value stored at adress

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - this function changes the value of a variable a adress points
+ * to to 98
+ *
+ * @n: this is the pointer to a value of another variable
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
