5a3c13eb779be2c47cf54b40f1b4d26731b2d537
Nick <greywhat2002@gmail.com>
2023-07-24 11:10:29 -0700

    function print last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * puts_half - this function prints only half a string
+ *
+ * @str: string to be evaluated
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int leng;
+	int un_half;
+	int half;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	leng = i - 1;
+	half = leng / 2;
+	un_half = (leng - 1) / 2;
+
+	if (leng % 2 == 0)
+	{
+		while (half <= leng)
+		{
+			_putchar(str[half] + 1);
+			half++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		while (un_half <= leng)
+		{
+			_putchar(str[un_half + 1]);
+			un_half++;
+		}
+		_putchar('\n');
+	}
+}
