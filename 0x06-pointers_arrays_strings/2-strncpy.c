0fe3a0f6a6b72e1b685951263a1d3bf985036aff
Nick <greywhat2002@gmail.com>
2023-07-27 06:17:54 -0700

    betty fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
+ * _strncpy - this function copies n elements of a string to another string
  *
+ * @dest: is the string to be copied too
+ * @src: is the string to be copied
+ * @n: is the number of elements to be copied
  *
- *
- *
- *
- *
- *
+ * Return: this function returns the copied string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
