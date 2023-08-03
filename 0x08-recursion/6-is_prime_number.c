2f9d84aaa748d86586b7648cb86917b7abb05cb1
Nick <greywhat2002@gmail.com>
2023-08-03 06:07:48 -0700

    functions determines if a given number is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * is_prime_number - function checks if number is a prime number
+ *
+ * @n: number to be evaluated
+ *
+ * Return: return 0 if not a prime, 1 if it is a prime
+ */
+int checker(int n, int a);
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (checker(n , n -1));
+}
+/**
+ * checker - function determines if number has more than two functions
+ *
+ * @n: prime number
+ * @a: factor
+ *
+ * Return: return 0 if not a prime, 1 if true
+ */
+int checker(int n, int a)
+{
+	if (a == 1)
+		return (1);
+	if (n % a == 0 && a > 0)
+		return (0);
+	return (checker(n , a -1));
+}
