dbb70c7d37b4f3a7dd57859c26b25783e2655dea
Nick <greywhat2002@gmail.com>
2023-08-08 04:30:56 -0700

    fixed function to include cases where argument strings are NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,14 +18,14 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 	counter = 0;
 
-	while (s1[i])
+	while (s1 && s1[i])
 	{
 		i++;
 	}
 	count = i;
 	j = 0;
 
-	while (s2[j])
+	while (s2 && s2[j])
 	{
 		j++;
 	}
