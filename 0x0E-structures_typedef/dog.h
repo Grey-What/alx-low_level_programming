b09645fd2177dd3476112f00c560fe84e2b4a10f
Nick <greywhat2002@gmail.com>
2023-08-17 04:12:50 -0700

    defined a new type struct dog in header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains detail about a dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ *
+ * Description: this structure contains details about a dog , such as name of 
+ * owner, age and name of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
