f6fe30a4ba56142a5ac9462a700e344b4f1270ad
Nick <greywhat2002@gmail.com>
2023-08-18 03:14:16 -0700

    function prints numbers in argument separated by string from argument

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function print numbers in argument separated by s string
+ *
+ * @separator: string inbetween numbers
+ * @n: number of arguments
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int x;
+
+	va_list arg;
+
+	if (separator == NULL)
+		return;
+
+	va_start(arg, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(arg, int);
+
+		if (i == 0)
+			printf("%d", x);
+		else
+			printf("%s%d", separator, x);
+	}
+	printf("\n");
+	va_end(arg);
+}
