0b79f4713b80d73eab15f771d3626782e7195ae9
Nick <greywhat2002@gmail.com>
2023-08-18 04:22:30 -0700

    function prints strings from arguments separated b a string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - function prints strings separated by a string
+ *
+ * @separator: the separator string
+ * @n: number of arguments
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char *);
+
+		if (s == NULL)
+			s = "(nil)";
+
+		else if (separator == NULL)
+			printf("%s", s);
+		else if (i == 0 && separator)
+			printf("%s", s);
+		else
+			printf("%s%s", separator, s);
+	}
+	va_end(args);
+	printf("\n");
+}
