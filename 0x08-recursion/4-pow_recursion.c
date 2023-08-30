d26135a5749a34ef7e2bd92b9d77b88a1ba028b3
Grey-What <greywhat2002@gmail.com>
2023-08-30 06:48:32 -0700

    function determines value to the power of another

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _pow_recursion - function determines a value to the power of another value
+ *
+ * @x: base number
+ * @y: power
+ *
+ * Return: x to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
