54b942f206cbfdf719483f89935398b314c44813
Grey-What <greywhat2002@gmail.com>
2023-08-30 06:01:23 -0700

    function prints string with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,14 @@
  */
 void _puts_recursion(char *s)
 {
-  if (*s != '\0')
-    {
-      _putchar(*s);
-    }
-  _puts_recursions(s++);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
