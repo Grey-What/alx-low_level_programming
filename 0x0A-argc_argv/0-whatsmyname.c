45b822c70f40d6cee1f9aa29d5685e6245faaa85
Grey-What <greywhat2002@gmail.com>
2023-09-01 07:32:09 -0700

    created function that always print current name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function prints program name
+ *
+ * @argc: number of arguments
+ * @argv: araay of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
