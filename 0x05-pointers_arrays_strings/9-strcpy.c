af458a19ed06962b83bc17dbe93e422320f65d84
Grey-What <greywhat2002@gmail.com>
2023-09-08 06:06:42 -0700

    function copies a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcpy - function copies a string
+ *
+ * @dest: copied string
+ * @src: original string
+ *
+ * return: pointer to copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
