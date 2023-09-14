c651067cf9d4271a0fd242fa16fc89c25347375c
Grey-What <greywhat2002@gmail.com>
2023-09-14 06:51:21 -0700

    function prints integer arguments separated by a string

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function prints it's integer arguments
+ *
+ * @separator: string to be printed between numbers
+ * @n: numbe rof arguments
+ *
+ * Description: function prints each of it's integer arguments separated by
+ * a string
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list nums;
+
+	va_start(nums, n);
+
+	if (n == 0)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		if (!separator || i == 0)
+		{
+			printf("%d", va_arg(nums, int));
+		}
+		else
+		{
+			printf("%s%d", separator, va_arg(nums, int));
+		}
+	}
+	printf("\n");
+}
