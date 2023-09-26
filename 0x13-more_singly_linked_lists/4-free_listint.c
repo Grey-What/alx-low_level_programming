3b314c287ce9f69a61d6b91b14b4f5537b60305f
Grey-What <greywhat2002@gmail.com>
2023-09-26 03:59:04 -0700

    removed previous change

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,14 +10,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	if (!head)
-		free(head);
-
 	while (head->next)
 	{
 		temp = head->next;
 		free(head);
 		head = temp;
 	}
-	free(head);
 }
