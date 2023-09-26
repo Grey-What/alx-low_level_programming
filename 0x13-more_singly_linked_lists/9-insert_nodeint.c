fc98f12c9ec87f880d86305a5a3d9bdbf94dbcb0
Grey-What <greywhat2002@gmail.com>
2023-09-26 06:34:21 -0700

    function creates and adds new node at specified index in linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - creates and add node at specified index in list
+ *
+ * @head: pointer to pointer to head of list
+ * @idx: index to add new node
+ * @n: data to intialize element of new node
+ *
+ * Return: NULL on failure else adress of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *new;
+	listint_t *hold = *head;
+
+	new = malloc(sizeof(listint_t));
+
+	if (!new || !hold)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = hold->next;
+		hold = new;
+		return (new);
+	}
+
+	while (hold && i < idx)
+	{
+		if (i == idx - 1)
+		{
+			new->next = hold->next;
+			hold->next = new;
+			return (new);
+		}
+		else
+			hold = hold->next;
+		i++;
+	}
+	return (NULL);
+}
