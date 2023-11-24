2e5dc0a5d8cb111ab7d92119d541bf83fc905731
Grey-What <greywhat2002@gmail.com>
2023-11-24 05:58:32 -0800

    function converts binary to int using bit operations

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * binary_t_uint - function converts binaary to unsigned int
+ *
+ * @b: binary to be converted
+ *
+ * Return: unint number or 0 if binary number is not valid
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	size_t num = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+	if (b[i] != '0' && b[i] != '1')
+		return (0);
+	}
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num = num << 1;
+		if (b[i] == '1')
+		{
+			num += 1;
+		}
+	}
+	return (num);
+}
