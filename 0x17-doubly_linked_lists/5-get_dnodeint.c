1056fb4e6f15be83404b8ada9e22ea6475734c31
Grey-What <greywhat2002@gmail.com>
2024-01-12 04:59:05 -0800

    returns the nth node of a dlistint_t linked list.

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - returns nth node of function
+ *
+ * @head: head of doubly linked list
+ * @index: nth node to be found
+ *
+ * Return: nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int i = 0, count = 0;
+
+	while (temp)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (count < index)
+		return (NULL);
+
+	while (i <= index)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
