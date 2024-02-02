a57f0024078f60803d4d7dbdf48328d19c42f7f2
Grey-What <greywhat2002@gmail.com>
2024-02-02 04:58:03 -0800

    fixed typos

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,9 +3,9 @@
  * key_index - gets index of key inside hashtable using djb2 algorithm
  *
  * @key: key to find index for
- * @sze: size of array in hash table
+ * @size: size of array in hash table
  *
- * Retur: index where key value pair should be stored
+ * Return: index where key value pair should be stored
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
